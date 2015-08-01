b21af9b20df608f665c42d91aec478387675f880
Pras Velagapudi <psigen@gmail.com>
2015-07-31 20:35:57 -0400

    Rearranged board.h to allow access to half-duplex pin defs.

--- src/platypus/Board.h
+++ src/platypus/Board.h
@@ -41,6 +41,7 @@ struct MultiPort
   int pwr_enable;
   int pwr_current;
   int analog;
+  int half_duplex;
 };
 
 enum GPIO
@@ -59,6 +60,17 @@ const Version VERSION = {4, 1, 0}
 // Pin definitions.
 const Led LED = { 54, 55, 56 };
 
+const int HALF_DUPLEX01 = 38;
+const int HALF_DUPLEX23 = 39;
+
+const int V_BATT = A3;
+const int CHG_CTRL = 51;
+
+const int LOOPBACK = 40;
+
+const int PWR_INT = 44;
+const int PWR_KILL = 46;
+
 const size_t NUM_DRIVE_PORTS = 2;
 
 const DrivePort DRIVE_PORTS[NUM_DRIVE_PORTS] = {
@@ -69,21 +81,11 @@ const DrivePort DRIVE_PORTS[NUM_DRIVE_PORTS] = {
 const size_t NUM_MULTI_PORTS = 4;
 
 const MultiPort MULTI_PORTS[NUM_MULTI_PORTS] = {
-  { 32, 30, 36, 34, { 21, 2, 20, 3 }, 66,  A8, A7 }, // Sensor 0
-  { 33, 31, 37, 35, { 19, 4, 18, 5 }, 67,  A9, A6 }, // Sensor 1
-  { 24, 22, 28, 26, { 17, 6, 16, 7 }, 68, A10, A5 }, // Sensor 2
-  { 25, 23, 29, 27, { 15, 8, 14, 9 }, 69, A11, A4 }  // Sensor 3
+  { 32, 30, 36, 34, { 21, 2, 20, 3 }, 66,  A8, A7, HALF_DUPLEX01 }, // Sensor 0
+  { 33, 31, 37, 35, { 19, 4, 18, 5 }, 67,  A9, A6, HALF_DUPLEX01 }, // Sensor 1
+  { 24, 22, 28, 26, { 17, 6, 16, 7 }, 68, A10, A5, HALF_DUPLEX23 }, // Sensor 2
+  { 25, 23, 29, 27, { 15, 8, 14, 9 }, 69, A11, A4, HALF_DUPLEX23 }  // Sensor 3
 };
-
-const int V_BATT = A3;
-const int CHG_CTRL = 51;
-
-const int HALF_DUPLEX01 = 38;
-const int HALF_DUPLEX23 = 39;
-const int LOOPBACK = 40;
-
-const int PWR_INT = 44;
-const int PWR_KILL = 46;
 
 // TODO: Switch to using HardwareSerial.
 USARTClass *MULTI_SERIAL_PORTS[NUM_MULTI_PORTS] = {
